fdd58d9b3df89ccdd19f35533a6dcff498502130
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2023-11-19 11:28:18 -0700

    Update metadata.ts

--- utils/metadata.ts
+++ utils/metadata.ts
@@ -8,7 +8,7 @@ export function createMetadata(override: Metadata): Metadata {
             description: override.description ?? undefined,
             url: 'https://help.cordx.lol',
             images: '/banner.png',
-            siteName: 'Next Docs',
+            siteName: 'CordX Docs',
             ...override.openGraph
         },
         twitter: {
